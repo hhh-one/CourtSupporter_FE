2d7d916bd8a9712ca11b413db6f7a0e9f84c9303
hhh-one <904lhw@naver.com>
2023-09-24 21:23:48 +0900

    chore: update build file

--- deploy_fe/deploy.sh
+++ deploy_fe/deploy.sh
@@ -8,6 +8,6 @@ docker rmi courtsupporter-fe-image
 
 source ~/.bashrc
 
-docker build -t courtsupporter-fe-image . \
+docker build -t courtsupporter-fe-image . 
 
 docker run -d -p 3000:3000 --name courtsupporter-fe courtsupporter-fe-image
